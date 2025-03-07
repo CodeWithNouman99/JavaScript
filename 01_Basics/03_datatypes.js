47394f5c74de40c320a9cfcdd9591257f6f31f01
Muhammad Nouman <mrnoumannomi7@gmail.com>
2025-03-07 16:46:35 +0000

    03_datatypes

---
+++ 01_Basics/03_datatypes.js
@@ -0,0 +1,36 @@
+"use strict "                        //treat all js code with new version
+
+
+//alert(3+3)                              //we are using node js,not  browser 
+   
+
+
+//onsole.log(3+35)  //we CAN Write something in the same line but this is not good practice coz of readability 
+
+let name="junaid"  //string
+let age =18 // number
+let isLoggedIn=false //boolean 
+let state=null;
+
+
+
+
+ //Following are the the primitive datatype
+
+
+// number => 2 to the power 53
+//bigint 
+//string =>""
+// boolean => T  F
+// null standalone value 
+//undefined  =>
+   // Symbol  => unique
+
+
+//Following are the non primitive data types
+
+
+
+//object  
+console.log(typeof undefined); //undefined 
+console.log(typeof null); //object
