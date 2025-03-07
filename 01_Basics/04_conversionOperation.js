92e8497bfcbc5a3e506c80fdc6006d0e8e12e954
Muhammad Nouman <mrnoumannomi7@gmail.com>
2025-03-07 16:47:04 +0000

    04_conversionOperation

---
+++ 01_Basics/04_conversionOperation.js
@@ -0,0 +1,38 @@
+let score= null;
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueINNumber= Number(score)
+console.log( valueINNumber);
+
+// "33" => 33
+//  "33abc" => Nan (Not a number)
+// "true" => 1,                               "false"=> 0
+
+
+
+
+let isloggedin= 1         //output is true     if declare 0 then output is false
+
+let booleanIsloggedIn= Boolean(isloggedin)
+console.log(booleanIsloggedIn);
+
+
+
+
+
+
+// let isloggedin= ""  // output is false
+
+
+// let isloggedin="nouman"       output is true
+
+// let booleanIsloggedIn= Boolean(isloggedin)
+// console.log(booleanIsloggedIn);
+
+
+let someNumber=33
+let stringNumber= String(someNumber)
+console.log(stringNumber);               //outupt is 33
+
+console.log( typeof stringNumber);          //outupt is string
