bb24772bcd06d336b8ec704e84eb8589700608bd
Muhammad Nouman <mrnoumannomi7@gmail.com>
2025-03-07 16:00:45 +0000

    02_variables

---
+++ 01_Basics/02_variables.js
@@ -0,0 +1,18 @@
+const accountId=1234556
+let accountEmail="mrnoumannomi7@gmail.com"
+var accountPassword="928579"
+accountCity="Lahore"
+ let accountState;    // we can also declare variables without intialize any value and we can receive output "Undefined"
+// accountId =2
+// accountEmail= junaid
+// accountPassword=789
+// console.log(accountId);
+/*
+This is a block
+*/
+
+
+/*
+Prefer not to use var coz of issue in block scope and functional scope
+*/
+console.table([accountId,accountEmail,accountPassword,accountState])
