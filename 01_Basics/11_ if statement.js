2886d41ef107d53505912fcebb3bb34cee4d1de4
Muhammad Nouman <mrnoumannomi7@gmail.com>
2025-03-27 02:25:41 +0500

    11_ if statement.js

---
+++ 01_Basics/11_ if statement.js
@@ -0,0 +1,17 @@
+const userName= prompt("Please enter your name")
+const userAge= prompt("Please enter your age")
+
+console.log(Name: ${userName});
+console.log(Age: ${userAge});
+
+if(userAge>=25 && userAge<="45"){
+    console.log("user is working professional");   
+    console.log("This is the if block");
+}
+console.log("Program ended");
+
+if(userName==cancel && userAge== cancel)
+{
+    userName= null
+    userAge=Null
+}
