df96605f347e4338878731f84b532457dd8788a9
Muhammad Nouman <mrnoumannomi7@gmail.com>
2025-04-03 01:00:52 +0500

    Arrays.js

---
+++ 01_Basics/Arrays.js
@@ -0,0 +1,5 @@
+***Arrays in JavaScript:
+
+An array in JavaScript is an ordered, indexed collection of values (of any type: numbers, strings, objects, even other arrays). Arrays are mutable (can be modified after creation) and provide built-in methods for manipulation.
+
+ *** Typeof "arrays" is "Object"
