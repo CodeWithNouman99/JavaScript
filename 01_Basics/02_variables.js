afd4c44cd94865fa5a92f5dd04bf4057500f15a1
Muhammad Nouman <mrnoumannomi7@gmail.com>
2025-04-24 01:11:58 +0500

    02_variables

--- 01_Basics/02_variables.js
+++ 01_Basics/02_variables.js
@@ -3,10 +3,10 @@ let accountEmail="mrnoumannomi7@gmail.com"
 var accountPassword="928579"
 accountCity="Lahore"
  let accountState;    // we can also declare variables without intialize any value and we can receive output "Undefined"
-// accountId =2
-// accountEmail= junaid
-// accountPassword=789
-// console.log(accountId);
+ accountId =2
+ accountEmail= junaid
+ accountPassword=789
+ console.log(accountId);
 /*
 This is a block
 */
