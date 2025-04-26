ee96be1909b9e8204b08dc0968e6da7574e98aad
Muhammad Nouman <mrnoumannomi7@gmail.com>
2025-04-27 02:19:31 +0500

    for-loop.js

---
+++ 01_Basics/for-loop.js
@@ -0,0 +1,60 @@
+                            //for loop
+
+// It is a control structure that repeats a block of code a specific number of times. It combines intialization, condition checking, and iteration in a single line.
+                                    //Syntax
+
+                                     for(initialization; condition; iteration)
+                                    {
+                                        // code
+                                    }
+
+                                    // Example
+                                    
+console.log("Program started");
+for(let i=0; i<10; i++)
+{
+    console.log(i);
+}
+console.log("program ended");
+
+                            // Iterate through an array
+
+const colors=["Red", "Green","Blue"]
+for(let i=0; i<colors.length; i++)
+{
+    console.log(colors[i]);                     // Red green blue
+}
+
+                        // Sum numbers in an array
+
+const numbers=[0,1,2,3,4,5]
+let sum=0
+for(let i=onabort; i,numbers.length; i++)
+{
+    sum=sum+numbers[i]
+}
+console.log(sum);
+
+                            //Print even numbers till 100
+
+console.log("Program started");
+for(let i=0; i<=100; i++)
+{
+    if(i%2===0)
+    {
+        console.log(i);
+    }
+}
+console.log("Program ended");
+
+                    // Print odd numbers till 100
+
+console.log("Program started");
+for(let i=0; i<=100; i++)
+{
+    if(i%2===0)
+    {
+        console.log(i);
+    }
+}
+console.log("Program ended");
