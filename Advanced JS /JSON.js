4bf3ae130b454d2024bb66408bf5e4dda1f13f99
Muhammad Nouman <mrnoumannomi7@gmail.com>
2025-07-24 08:51:59 +0500

    JSON.js

---
+++ Advanced JS /JSON.js
@@ -0,0 +1,25 @@
+                                                    JSON:
+JSON (JavaScript Object Notation):
+JSON is a lightweight data format used to store and exchange data between a server and a client, or between two programs. It is based on JavaScript object syntax but is used in many programming languages.
+Note:  
+      =>JSON be 1 object hi ha difference ye ha k object ka koi name hota ha means jo uper course kikha ha and JSON me keys ko string me likhta ha.
+      =>In JSON, keys must be in double quotes "like this"
+      =>It is commonly used with JavaScript through:
+      =>JSON.stringify() → Converts object to JSON string
+
+JSON.parse() → Converts JSON string back to object
+Example:
+{
+  "name": "Nouman",
+  "age": 24,
+  "isStudent": true
+}
+
+Key Features:
+1)Stores data in key-value pairs
+
+2)Data is written as strings, numbers, booleans, arrays, or objects
+
+3) Used in APIs, web apps, databases, and configuration files
+
+4)Easy to read and write for humans and machines
